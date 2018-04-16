6d1bf82abc995066aeb8905ebc57f039513b44c1
Keerthi4308 <keerthi4308@gmail.com>
2018-04-16 08:55:48 -0400

    glance service

---
+++ swagger-docker/openstack/admin-openrc.sh
@@ -0,0 +1,7 @@
+export OS_USERNAME="nkeerthi"
+export OS_PASSWORD="Flower@11"
+export OS_PROJECT_ID="CH-819337"
+export os_PROJECT_NAME="CH-819337"
+export OS_AUTH_URL="https://openstack.tacc.chameleoncloud.org:5000/v2.0"
+export OS_IDENTITY_API_VERSION="2"
+
---
+++ swagger-docker/openstack/listimg.py
@@ -0,0 +1,15 @@
+from os import environ as env
+from credentials import get_keystone_creds
+import keystoneclient.v2_0.client as ksclient
+import glanceclient
+
+
+def get_images_list():
+ 
+    creds =get_keystone_creds()
+    keystone=ksclient.Client(**creds)
+    glance_endpoint = keystone.service_catalog.url_for(service_type='image', endpoint_type='publicURL')
+    glance = glanceclient.Client('2',glance_endpoint, token=keystone.auth_token)
+    images = glance.images.list()
+
+return(images)
