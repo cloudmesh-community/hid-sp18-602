4c4c46d14162a5be3b1f852d8c3139c9baa2c3d3
Keerthi4308 <keerthi4308@gmail.com>
2018-04-17 17:58:37 -0400

    error in listimg.py resolved

--- swagger-docker/openstack/credentials.py
+++ swagger-docker/openstack/credentials.py
@@ -6,4 +6,8 @@ def get_keystone_creds():
     d['username'] = os.environ['OS_USERNAME']
     d['password'] = os.environ['OS_PASSWORD']
     d['auth_url'] = os.environ['OS_AUTH_URL']
+    d['project_name']=os.environ['OS_PROJECT_NAME']
+    d['project_id']=os.environ['OS_PROJECT_ID']
+    d['identity_version']=os.environ['OS_IDENTITY_API_VERSION']
+	
     return d
--- swagger-docker/openstack/listimg.py
+++ swagger-docker/openstack/listimg.py
@@ -8,8 +8,8 @@ def get_images_list():
  
     creds =get_keystone_creds()
     keystone=ksclient.Client(**creds)
-    glance_endpoint = keystone.service_catalog.url_for(service_type='image', endpoint_type='publicURL')
-    glance = glanceclient.Client('2',glance_endpoint, token=keystone.auth_token)
+    glance_endpoint = keystone.service_catalog.url_for(service_type='image', endpoint_type='public')
+    glance = glclient.Client(glance_endpoint, token=keystone.auth_token)
     images = glance.images.list()
-
-return(images)
+    return images
+    
