0ec6133f17545f77609e701fadafb5d3684923a3
Keerthi4308 <keerthi4308@gmail.com>
2018-04-18 02:54:44 -0400

    error in listimg.py is fixed

--- swagger-docker/openstack/listimg.py
+++ swagger-docker/openstack/listimg.py
@@ -11,5 +11,11 @@ def get_images_list():
     glance_endpoint = keystone.service_catalog.url_for(service_type='image', endpoint_type='public')
     glance = glclient.Client(glance_endpoint, token=keystone.auth_token)
     images = glance.images.list()
-    return images
+    detailed_list =list(images)
+    images_name=[]
+    for x in detailed_list:
+	if(x["status"]=="active" and x["visibility"]=="public"):
+	   images_name.append([x["name"],x["id"]])
+    images_name.sort()
+    return images_name
     
