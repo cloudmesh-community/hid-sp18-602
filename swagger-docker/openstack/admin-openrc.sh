821063de248de0a297259de06bd5ab5fdbcccd62
Keerthi4308 <keerthi4308@gmail.com>
2018-04-28 15:50:54 -0400

    password cred removed from admin-openrc.sh

--- swagger-docker/openstack/admin-openrc.sh
+++ swagger-docker/openstack/admin-openrc.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 export OS_USERNAME="nkeerthi"
-export OS_PASSWORD="Flower@11"
 export OS_PROJECT_ID="CH-819337"
-export os_PROJECT_NAME="CH-819337"
+export OS_PROJECT_NAME="CH-819337"
 export OS_AUTH_URL="https://openstack.tacc.chameleoncloud.org:5000/v2.0"
+echo "please enter your  openstack password"
+read -sr OS_PASSWORD_INPUT
+export OS_PASSWORD=$OS_PASSWORD_INPUT
 export OS_IDENTITY_API_VERSION="2"
+2"
 
